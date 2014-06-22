5746289de01a348358175e35dc754d4bb523aeed
François Pluchino <francois.pluchino@gmail.com>
2014-06-22 13:37:07 +0200

    Adds custom phpunit bootstrap

---
+++ Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This file is part of the Fxp Composer Asset Plugin package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/composer/composer/tests/Composer/TestCase.php';
