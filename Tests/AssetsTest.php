ba3ca0b639c52ac27351790319b2f9804e416eb7
François Pluchino <francois.pluchino@gmail.com>
2014-07-02 13:06:45 +0200

    Adds assets tests

---
+++ Tests/AssetsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Fxp Composer Asset Plugin package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fxp\Composer\AssetPlugin\Tests;
+
+use Fxp\Composer\AssetPlugin\Assets;
+
+/**
+ * Tests of assets factory.
+ *
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+class AssetsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetTypes()
+    {
+        $this->assertEquals(array(
+                "npm",
+                "bower"
+            ), Assets::getTypes());
+    }
+
+    public function testCreationOfInvalidType()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+
+        Assets::createType(null);
+    }
+
+    public function testCreationOfNpmAsset()
+    {
+        $type = Assets::createType('npm');
+
+        $this->assertInstanceOf('Fxp\Composer\AssetPlugin\Type\AssetTypeInterface', $type);
+    }
+
+    public function testCreationOfBowerAsset()
+    {
+        $type = Assets::createType('bower');
+
+        $this->assertInstanceOf('Fxp\Composer\AssetPlugin\Type\AssetTypeInterface', $type);
+    }
+}
