053ce17bb58b40423b554607c5beedd32c495c1d
François Pluchino <francois.pluchino@gmail.com>
2014-07-03 18:55:23 +0200

    Adds bower type tests

---
+++ Tests/Type/BowerAssetTypeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Fxp Composer Asset Plugin package.
+ *
+ * (c) François Pluchino <francois.pluchino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fxp\Composer\AssetPlugin\Tests\Type;
+
+use Fxp\Composer\AssetPlugin\Type\BowerAssetType;
+
+/**
+ * Tests of bower asset type.
+ *
+ * @author François Pluchino <francois.pluchino@gmail.com>
+ */
+class BowerAssetTypeTest extends AbstractAssetTypeTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->type = new BowerAssetType($this->packageConverter, $this->versionConverter);
+    }
+
+    public function testInformations()
+    {
+        $this->assertSame('bower', $this->type->getName());
+        $this->assertSame('bower-asset', $this->type->getComposerVendorName());
+        $this->assertSame('bower.json', $this->type->getFilename());
+    }
+}
