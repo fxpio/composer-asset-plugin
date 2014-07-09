d33447ea778cec146d0d0880b51b5f2d406b2342
Martin Hasoň <martin.hason@gmail.com>
2014-07-09 17:31:37 +0200

    Added support for "ignore" in bower.json

--- Installer/BowerInstaller.php
+++ Installer/BowerInstaller.php
@@ -11,6 +11,8 @@
 
 namespace Fxp\Composer\AssetPlugin\Installer;
 
+use Composer\Package\PackageInterface;
+
 /**
  * Installer for bower packages
  *
@@ -18,4 +20,43 @@ namespace Fxp\Composer\AssetPlugin\Installer;
  */
 class BowerInstaller extends AssetInstaller
 {
+    /**
+     * {@inheritdoc}
+     */
+    protected function installCode(PackageInterface $package)
+    {
+        parent::installCode($package);
+
+        $this->deleteIgnoredFiles($package);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function updateCode(PackageInterface $initial, PackageInterface $target)
+    {
+        parent::updateCode($initial, $target);
+
+        $this->deleteIgnoredFiles($target);
+    }
+
+    /**
+     * Deletes files defined in bower.json in section "ignore"
+     *
+     * @param PackageInterface $package
+     */
+    protected function deleteIgnoredFiles(PackageInterface $package)
+    {
+        $extra = $package->getExtra();
+        if (empty($extra['bower-asset-ignore'])) {
+            return;
+        }
+
+        $ignorer = new BowerIgnoreManager();
+        foreach ($extra['bower-asset-ignore'] as $pattern) {
+            $ignorer->addPattern($pattern);
+        }
+
+        $ignorer->deleteInDir($this->getInstallPath($package), $this->filesystem);
+    }
 }
