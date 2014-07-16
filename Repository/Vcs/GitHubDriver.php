a5223a66aea190ec4435ca8a493722a25b58ca6c
François Pluchino <francois.pluchino@gmail.com>
2014-07-16 18:44:41 +0200

    Optimize github driver

--- Repository/Vcs/GitHubDriver.php
+++ Repository/Vcs/GitHubDriver.php
@@ -37,51 +37,21 @@ class GitHubDriver extends BaseGitHubDriver
             return $this->gitDriver->getComposerInformation($identifier);
         }
 
-        if (preg_match('{[a-f0-9]{40}}i', $identifier) && $res = $this->cache->read($this->repoConfig['asset-type'] . '-' . $identifier)) {
+        $isSha = (bool) preg_match('{[a-f0-9]{40}}i', $identifier);
+
+        if ($isSha && $res = $this->cache->read($this->repoConfig['asset-type'] . '-' . $identifier)) {
             $this->infoCache[$identifier] = JsonFile::parseJson($res);
         }
 
         if (!isset($this->infoCache[$identifier])) {
-            $notFoundRetries = 2;
-            $resource = null;
-            $composer = null;
-            while ($notFoundRetries) {
-                try {
-                    $resource = $this->getApiUrl() . '/repos/'.$this->owner.'/'.$this->repository.'/contents/' . $this->repoConfig['filename'] . '?ref='.urlencode($identifier);
-                    $composer = JsonFile::parseJson((string) $this->getContents($resource));
-                    if (empty($composer['content']) || $composer['encoding'] !== 'base64' || !($composer = base64_decode($composer['content']))) {
-                        throw new \RuntimeException('Could not retrieve ' . $this->repoConfig['filename'] . ' from '.$resource);
-                    }
-                    break;
-                } catch (TransportException $e) {
-                    if (404 !== $e->getCode()) {
-                        throw $e;
-                    }
-
-                    // retry fetching if github returns a 404 since they happen randomly
-                    $notFoundRetries--;
-                    $composer = false;
-                }
-            }
+            $resource = $this->getApiUrl() . '/repos/'.$this->owner.'/'.$this->repository.'/contents/' . $this->repoConfig['filename'] . '?ref='.urlencode($identifier);
+            $composer = $this->getComposerContent($resource);
 
             if ($composer) {
-                $composer = JsonFile::parseJson($composer, $resource);
-
-                if (!isset($composer['time'])) {
-                    $resource = $this->getApiUrl() . '/repos/'.$this->owner.'/'.$this->repository.'/commits/'.urlencode($identifier);
-                    $commit = JsonFile::parseJson((string) $this->getContents($resource), $resource);
-                    $composer['time'] = $commit['commit']['committer']['date'];
-                }
-                if (!isset($composer['support']['source'])) {
-                    $label = array_search($identifier, $this->getTags()) ?: array_search($identifier, $this->getBranches()) ?: $identifier;
-                    $composer['support']['source'] = sprintf('https://%s/%s/%s/tree/%s', $this->originUrl, $this->owner, $this->repository, $label);
-                }
-                if (!isset($composer['support']['issues']) && $this->hasIssues) {
-                    $composer['support']['issues'] = sprintf('https://%s/%s/%s/issues', $this->originUrl, $this->owner, $this->repository);
-                }
+                $composer = $this->convertComposerContent($composer, $resource, $identifier);
             }
 
-            if (preg_match('{[a-f0-9]{40}}i', $identifier)) {
+            if ($isSha) {
                 $this->cache->write($this->repoConfig['asset-type'] . '-' . $identifier, json_encode($composer));
             }
 
@@ -89,6 +59,72 @@ class GitHubDriver extends BaseGitHubDriver
         }
 
         return $this->infoCache[$identifier];
+    }
+
+    /**
+     * Gets content of composer information.
+     *
+     * @param string $resource
+     *
+     * @return bool|null|string
+     *
+     * @throws \RuntimeException
+     * @throws \Composer\Downloader\TransportException
+     * @throws \Exception
+     */
+    protected function getComposerContent($resource)
+    {
+        $notFoundRetries = 2;
+        $composer = null;
+
+        while ($notFoundRetries) {
+            try {
+                $composer = JsonFile::parseJson((string) $this->getContents($resource));
+                if (empty($composer['content']) || $composer['encoding'] !== 'base64' || !($composer = base64_decode($composer['content']))) {
+                    throw new \RuntimeException('Could not retrieve ' . $this->repoConfig['filename'] . ' from '.$resource);
+                }
+                break;
+            } catch (TransportException $e) {
+                if (404 !== $e->getCode()) {
+                    throw $e;
+                }
+
+                // retry fetching if github returns a 404 since they happen randomly
+                $notFoundRetries--;
+                $composer = false;
+            }
+        }
+
+        return $composer;
+    }
+
+    /**
+     * Converts json composer file to array.
+     *
+     * @param string $composer
+     * @param string $resource
+     * @param string $identifier
+     *
+     * @return array
+     */
+    protected function convertComposerContent($composer, $resource, $identifier)
+    {
+        $composer = JsonFile::parseJson($composer, $resource);
+
+        if (!isset($composer['time'])) {
+            $resource = $this->getApiUrl() . '/repos/'.$this->owner.'/'.$this->repository.'/commits/'.urlencode($identifier);
+            $commit = JsonFile::parseJson((string) $this->getContents($resource), $resource);
+            $composer['time'] = $commit['commit']['committer']['date'];
+        }
+        if (!isset($composer['support']['source'])) {
+            $label = array_search($identifier, $this->getTags()) ?: array_search($identifier, $this->getBranches()) ?: $identifier;
+            $composer['support']['source'] = sprintf('https://%s/%s/%s/tree/%s', $this->originUrl, $this->owner, $this->repository, $label);
+        }
+        if (!isset($composer['support']['issues']) && $this->hasIssues) {
+            $composer['support']['issues'] = sprintf('https://%s/%s/%s/issues', $this->originUrl, $this->owner, $this->repository);
+        }
+
+        return $composer;
     }
 
     /**
