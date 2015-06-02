9e6376180201266f9b3cab951cc0f8596df37a1c
Alexander Nestorov <alexandernst@gmail.com>
2015-06-02 22:32:21 +0200

    Add unit tests for #119

--- Tests/Converter/NpmPackageConverterTest.php
+++ Tests/Converter/NpmPackageConverterTest.php
@@ -81,6 +81,7 @@ class NpmPackageConverterTest extends AbstractPackageConverterTest
             'ASSET/test-library17-file' => '*',
             'ASSET/test-library18-file' => '1.2.3',
             'ASSET/test-library19-file' => '*',
+            'ASSET/library20' => '1 || 2',
         ), $composer['require']);
 
         $this->assertArrayHasKey('require-dev', $composer);
