6775aeecb361c9a0fe8d41c8d19eed6be24e705c
François Pluchino <francois.pluchino@gmail.com>
2016-05-27 11:54:00 +0200

    Fix encoding of special character

--- Repository/BowerPrivateRepository.php
+++ Repository/BowerPrivateRepository.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Fxp Composer Asset Plugin package.
  *
- * (c) François Pluchino <francois.pluchino@gmail.com>
+ * (c) FranÃ§ois Pluchino <francois.pluchino@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
