953b79711d45ddb6d90136f4001648785f660e4a
François Pluchino <francois.pluchino@gmail.com>
2017-09-07 10:49:05 +0200

    Add phpunit class alias in bootstrap test

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,4 +10,9 @@
  */
 
 require __DIR__.'/../vendor/autoload.php';
+
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
+
 require __DIR__.'/../vendor/composer/composer/tests/Composer/TestCase.php';
